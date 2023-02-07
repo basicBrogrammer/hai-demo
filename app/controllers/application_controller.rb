744c82417c1c6f35c604730315514e26ee6ce9bb
Jeremy Ward <jeremy.ward@shopify.com>
2023-02-07 10:25:34 -0500

    mount Hai::Rest::Engine

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    User.last
+  end
+
+  def context
+    { current_user: current_user }
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  module Policies
+    def self.update(record, context)
+      raise UnauthorizedError unless record.user == context[:current_user]
+    end
+  end
+  module Actions
+    def self.create(record, context)
+      record.user = context[:current_user]
+    end
+  end
 end
